d6f58661ecd1d011b59db2f2618c3cc6878c7079
Curt Manning <CMann4Surf@gmail.com>
2018-01-16 18:21:57 -0800

    First commit - Made it a java project in eclipse

---
+++ src/introduction/HelloWorld.java
@@ -0,0 +1,13 @@
+package introduction;
+/*
+ * This is a simple Java Program.
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// println prints a String, then terminate the line.. 
+		System.out.println("Hello World!");
+	}
+
+}
+
