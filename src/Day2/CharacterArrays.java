23971fa47a7f8adbecf0a1f701357708583f84f9
Curt Manning <CMann4Ski@yahoo.com>
2018-01-18 23:15:10 -0800

    Second Day Lecture

---
+++ src/Day2/CharacterArrays.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class CharacterArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c='a';
+		System.out.printf("%c%n" ,c);
+		char[] arr={'a','b','c','d'};
+		for(char ch: arr) {
+			System.out.printf("%c%n" ,ch);
+		}
+
+	}
+
+}
---
+++ src/Day2/LoopsExercise.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class LoopsExercise {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
