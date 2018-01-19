2b13567c8adbda95d854c963e04dc8eee42acde9
Curt Manning <CMann4Surf@gmail.com>
2018-01-19 01:43:42 -0800

    Completed Homework LoopExercise.java / Cleaned up and formated Task2.java

--- src/Day2/LoopsExercise.java
+++ src/Day2/LoopsExercise.java
@@ -1,13 +1,16 @@
 package Day2;
 
 public class LoopsExercise {
-
 	/**
-	 * @param args
+	 * Given an integer, n print its first 10 multiples. Each multiple should be
+	 * printed on a new line in the form: n x i = result.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int n = 2;
+		for (int i = 1; i <= 10; i++) {
+			System.out.format("%d x %d = %d%n", n, i, n * i);
+		}
 	}
 
 }
